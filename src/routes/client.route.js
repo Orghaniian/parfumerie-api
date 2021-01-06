2e0072a29d2b7a3a7c585b50b34d189db130f3c5
Orghaniian <e163100@univ-lemans.fr>
2021-01-06 16:14:30 +0100

    refacto

--- src/routes/client.route.js
+++ src/routes/client.route.js
@@ -4,6 +4,10 @@ const isEmail = require('../utils/isEmail');
 const express = require('express'),
     router = express.Router();
 
+const clientArticlesRouter = require("./testRouter")
+
+router.use("/test", clientArticlesRouter)
+
 // get client
 router.get('/:id', function (req, res) {
     let sql = `SELECT * FROM client WHERE Code_client = ${db.escape(req.params.id)}`;
@@ -27,30 +31,23 @@ router.get('/:id', function (req, res) {
 
 // créer nouveau client
 router.post('/', function (req, res) {
-    const keysToTest = ["nom"]
-    if(!keysToTest.every(key => Object.keys(req.body).includes(key))){
-        const missingKeys = keysToTest.filter(key => !Object.keys(req.body).includes(key))
-        throw new HttpException(400, "Paramètre(s) manquant", {parametresManquants: missingKeys})
-    }else if(req.body.email && !isEmail(req.body.email)){
-        throw new HttpException(400, "Email invalide", {emailInvalde: req.body.email})
-    }else{
-        let sql = `INSERT INTO client(nom, facebook, instagram, email, telephone) VALUES (?)`;
-        let values = [
-            req.body.nom,
-            req.body.facebook,
-            req.body.instagram,
-            req.body.email,
-            req.body.telephone
-        ];
-        db.query(sql, [values], function (err, data, fields) {
-            if (err) throw err;
-            res.json({
-                status: 200,
-                Code_client: data.insertId,
-                message: "Nouveau client ajouté avec succès"
-            })
+    testParamsValid(req)
+    let sql = `INSERT INTO client(nom, facebook, instagram, email, telephone) VALUES (?)`;
+    let values = [
+        req.body.nom,
+        req.body.facebook,
+        req.body.instagram,
+        req.body.email,
+        req.body.telephone
+    ];
+    db.query(sql, [values], function (err, data, fields) {
+        if (err) throw err;
+        res.json({
+            status: 200,
+            Code_client: data.insertId,
+            message: "Nouveau client ajouté avec succès"
         })
-    }
+    })
 });
 
 //update client
@@ -65,4 +62,15 @@ router.put('/:no', function (req, res) {
     })
 })
 
+
 module.exports = router;
+
+function testParamsValid(req){
+    const keysToTest = ["nom"]
+    if(!keysToTest.every(key => Object.keys(req.body).includes(key))){
+        const missingKeys = keysToTest.filter(key => !Object.keys(req.body).includes(key))
+        throw new HttpException(400, "Paramètre(s) manquant", {parametresManquants: missingKeys})
+    }else if(req.body.email && !isEmail(req.body.email)){
+        throw new HttpException(400, "Email invalide", {emailInvalde: req.body.email})
+    }
+}
