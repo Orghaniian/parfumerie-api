6141461d14b367268e7f8915a8e6263db2a5d9b6
Orghaniian <e163100@univ-lemans.fr>
2021-01-13 19:56:39 +0100

    ajout tri par nom

--- src/routes/articles.route.js
+++ src/routes/articles.route.js
@@ -18,16 +18,17 @@ orderBy croi/dec
 router.get('/', function (req, res) {
     let limit = isNumeric(req.query.limit) ? req.query.limit : 10;
     let sql = `SELECT * FROM article WHERE nom LIKE '${req.query.nom ? req.query.nom.trim() : ""}%' `;
+    console.log(req.query)
     if (req.query.echangeable) sql += " AND echangeable"
     if (req.query.cadeau) sql += " AND en_cadeau"
     if (req.query.disponible) sql += " AND disponible"
     if (req.query.echangeable) sql += " AND echangeable"
     if (req.query.stock) sql += " AND quantite_en_stock"
     if (req.query.orderBy) {
-        if (req.query.orderBy === "croi") sql += " ORDER BY Prix_unitaire ASC"
+        if (req.query.orderBy === "asc") sql += " ORDER BY Prix_unitaire ASC"
         else if (req.query.orderBy === "dec") sql += " ORDER BY Prix_unitaire DESC"
+        else if (req.query.orderBy === "nom") sql += " ORDER BY nom"
     }
-
     sql += ` LIMIT ${limit}`
     db.query(sql, function (err, data, fields) {
         if (err) throw err;
