88654e29841dd10a6e90b8d48144714fb3ff75eb
Orghaniian <e163100@univ-lemans.fr>
2021-01-13 19:56:49 +0100

    ajout tri

--- src/routes/clients.route.js
+++ src/routes/clients.route.js
@@ -6,7 +6,13 @@ const express = require('express'),
 // get clients, limite par défaut à 10
 router.get('/', function (req, res) {
     let limit = isNumeric(req.query.limit) ? req.query.limit : 10;
-    let sql = `SELECT * FROM client LIMIT ${limit}`;
+    let sql = `SELECT * FROM client WHERE nom LIKE '${req.query.nom ? req.query.nom.trim() : ""}%' `;
+    if (req.query.orderBy) {
+        if (req.query.orderBy === "asc") sql += " ORDER BY Points ASC"
+        else if (req.query.orderBy === "dec") sql += " ORDER BY Points DESC"
+        else if (req.query.orderBy === "nom") sql += " ORDER BY nom"
+    }
+    sql += ` LIMIT ${limit}`
     db.query(sql, function (err, data, fields) {
         if (err) throw err;
         res.json({
