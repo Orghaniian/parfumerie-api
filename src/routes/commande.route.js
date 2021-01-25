693f50ffa3fc0f551d85b514d9e134d4212432f4
Orghaniian <e163100@univ-lemans.fr>
2021-01-25 19:24:48 +0100

    Déduction quantité article

--- src/routes/commande.route.js
+++ src/routes/commande.route.js
@@ -27,14 +27,13 @@ router.get('/:id', function (req, res) {
 // créer nouvelle commande
 router.post('/', function (req, res) {
     if (!req.body.articles || req.body.articles.length === 0) throw new HttpException(400, "La commande doit contenir des articles", { articles: req.body.articles })
-
     const keysToTest = ["prix", "frais_livraison", "code_client"]
     if (!keysToTest.every(key => Object.keys(req.body).includes(key))) {
         const missingKeys = keysToTest.filter(key => !Object.keys(req.body).includes(key))
         throw new HttpException(400, "Paramètre(s) manquant", { parametresManquants: missingKeys })
     } else {
-        let sql = `INSERT INTO commande (Prix, Frais_livraison, Code_client ${req.body.date_commande ? ",Date_commande" : ""} ${req.body.statut ? ", statut" : ""} ) VALUES (?)`;
-        let values = [
+        const sql = `INSERT INTO commande (Prix, Frais_livraison, Code_client ${req.body.date_commande ? ",Date_commande" : ""} ${req.body.statut ? ", statut" : ""} ) VALUES (?)`;
+        const values = [
             req.body.prix,
             req.body.frais_livraison,
             req.body.code_client
@@ -45,9 +44,13 @@ router.post('/', function (req, res) {
             console.log("articles: ", req.body.articles)
             if (err) throw err;
             req.body.articles.forEach(article => {
-                const sql = `INSERT INTO commande_has_article (no_commande, no_article, quantite, prix_achat) VALUES (${data.insertId}, ${article.no_article}, ${article.quantite}, ${article.prix_achat})`
-                db.query(sql, function (err2, data2, fields2) {
-                    if (err2) throw err2;
+                const sql = `UPDATE article SET Quantite_en_stock = Quantite_en_stock - ${article.quantite} WHERE No_article = ${article.no}`;
+                db.query(sql, function (err3, data3, fields3){
+                    if (err3) throw err3;
+                    const sql = `INSERT INTO commande_has_article (no_commande, no_article, quantite, prix_achat) VALUES (${data.insertId}, ${article.no}, ${article.quantite}, ${article.prixUnit})`
+                    db.query(sql, function (err2, data2, fields2) {
+                        if (err2) throw err2;
+                    })
                 })
             })
             res.json({
