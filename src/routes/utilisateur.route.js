768d919e3bffcd78580a5d8f23f213318dae4cb7
Orghaniian <e163100@univ-lemans.fr>
2021-02-04 18:30:11 +0100

    inscription

---
+++ src/routes/utilisateur.route.js
@@ -0,0 +1,12 @@
+const HttpException = require('../utils/HttpException');
+
+const express = require('express'),
+    router = express.Router();
+
+//inscription
+router.post("/", function (req, res){
+    let sql = `INSERT INTO `
+})
+
+
+module.exports = router;
