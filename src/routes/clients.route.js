e2402a030bb58ae6510cbd09aea39725538796c8
MaiwennLeCacheux <74050437+MaiwennLeCacheux@users.noreply.github.com>
2021-02-06 18:43:45 +0100

    tri asc et desc et nom

--- src/routes/clients.route.js
+++ src/routes/clients.route.js
@@ -8,9 +8,9 @@ router.get('/', function (req, res) {
     let limit = isNumeric(req.query.limit) ? req.query.limit : 10;
     let sql = `SELECT * FROM client WHERE nom LIKE '${req.query.nom ? req.query.nom.trim() : ""}%' `;
     if (req.query.orderBy) {
-        if (req.query.orderBy === "asc") sql += " ORDER BY Points ASC"
-        else if (req.query.orderBy === "dec") sql += " ORDER BY Points DESC"
-        else if (req.query.orderBy === "nom") sql += " ORDER BY nom"
+        if (req.query.orderBy === "asc") sql += " ORDER BY Code_client ASC"
+        else if (req.query.orderBy === "dec") sql += " ORDER BY Code_client DESC"
+        else if (req.query.orderBy === "nom") sql += " ORDER BY Nom"
     }
     sql += ` LIMIT ${limit}`
     db.query(sql, function (err, data, fields) {
